54e38cc3d71b799be368f25ab7155abd1f521520
chandankalyan <chandankalyanrn@gmail.com>
2023-11-09 19:49:07 +0530

    orders

--- fast/main.py
+++ fast/main.py
@@ -4,24 +4,27 @@ from pydantic import BaseModel
 import models
 from database import engine, SessionLocal
 from sqlalchemy.orm import Session
+from sqlalchemy import Select
 
 
 app = FastAPI()
 models.Base.metadata.create_all(bind= engine)
 
-class PostBase(BaseModel):
-    title : str
-    content : str
-    user_id : int
 
 class Sign_in(BaseModel):
     username: str
     password: str
 
-class UserBase(BaseModel):
+class Sign_up(BaseModel):
     username: str
     email : str
     password: str
+
+class Order(BaseModel):
+    order_id : int
+    item_id : int
+    user_name : str
+    quantity : int
 
 def get_db():
     db = SessionLocal()
@@ -33,7 +36,7 @@ def get_db():
 db_dependency = Annotated[Session, Depends(get_db)]
 
 @app.post('/users/sign-up/{credentials}', status_code= status.HTTP_201_CREATED)
-async def create_user(user: UserBase, db: db_dependency):
+async def create_user(user: Sign_up, db: db_dependency):
     db_user = models.User(**user.dict())
     db.add(db_user)
     db.commit()
@@ -50,5 +53,13 @@ async def sign_into_account(credentials: Sign_in, db: db_dependency):
         else:
             raise HTTPException(status_code= 401, detail= 'Password is incorrect')
 
+@app.post('/users/order/{order_details}', status_code= status.HTTP_201_CREATED)
+async def make_order(details: Order, db: db_dependency):
+    itemName = db.query(models.menu.item_name).filter(models.menu.item_ID == details.item_id).first()
+    db_order = models.Orders(**details.dict())
+    db.add(db_order)
+    db.commit()
+    
+    return {'Order Confirmed': f'{itemName[0]}'}
 
     
