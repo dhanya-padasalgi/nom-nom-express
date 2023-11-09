ea408a6783da1af268c5e218e5fa8bc97358daa7
Dhanya Padasalgi <dhanyapadasalgi@gmail.com>
2023-11-09 09:31:34 -0800

    https integrated with SIGNLOG

--- nne1/lib/signlog.dart
+++ nne1/lib/signlog.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class LoginSignupPage extends StatefulWidget {
   @override
@@ -7,6 +9,12 @@ class LoginSignupPage extends StatefulWidget {
 
 class _LoginSignupPageState extends State<LoginSignupPage> {
   bool isLoginSelected = true;
+
+  // Local variables to store user input during signup
+  final TextEditingController usernameController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final TextEditingController confirmPasswordController = TextEditingController();
 
   void toggleSelection() {
     setState(() {
@@ -32,8 +40,8 @@ class _LoginSignupPageState extends State<LoginSignupPage> {
           children: <Widget>[
             Positioned(
               top: 100,
-              left: 30, // Add this line
-              right: 30, // Add this line
+              left: 30,
+              right: 30,
               child: Container(
                 width: 350,
                 decoration: BoxDecoration(
@@ -69,11 +77,10 @@ class _LoginSignupPageState extends State<LoginSignupPage> {
                 ),
               ),
             ),
-            // Column 2
             Positioned(
               top: 250,
-              left: 30, // Add this line
-              right: 30, // Add this line
+              left: 30,
+              right: 30,
               child: Column(
                 children: [
                   if (isLoginSelected) ...[
@@ -96,6 +103,18 @@ class _LoginSignupPageState extends State<LoginSignupPage> {
                     ElevatedButton(
                       onPressed: () {
                         // Implement signup logic here
+                        if (validateSignUpInput()) {
+                          // If input is valid, prepare the JSON payload
+                          final Map<String, String> signUpData = {
+                            'username': usernameController.text,
+                            'email': emailController.text,
+                            'password': passwordController.text,
+                          };
+                          final jsonData = json.encode(signUpData);
+
+                          // Send the HTTP request
+                          sendRequest(jsonData);
+                        }
                       },
                       style: ElevatedButton.styleFrom(
                         primary: primaryColor,
@@ -123,6 +142,7 @@ class _LoginSignupPageState extends State<LoginSignupPage> {
     return Container(
       margin: EdgeInsets.only(bottom: 16),
       child: TextField(
+        controller: TextEditingController(),
         decoration: InputDecoration(
           labelText: labelText,
           filled: true,
@@ -133,5 +153,40 @@ class _LoginSignupPageState extends State<LoginSignupPage> {
         ),
       ),
     );
+  }
+
+  bool validateSignUpInput() {
+    if (usernameController.text.isEmpty ||
+        emailController.text.isEmpty ||
+        passwordController.text.isEmpty ||
+        confirmPasswordController.text.isEmpty) {
+      return false;
+    } else if (passwordController.text != confirmPasswordController.text) {
+      return false;
+    }
+    return true;
+  }
+}
+
+Future<void> sendRequest(String jsonData) async {
+  final url = Uri.parse('http://localhost:8000/your-api-endpoint');
+
+  final headers = {'Content-Type': 'application/json'};
+
+  try {
+    final response = await http.post(
+      url,
+      headers: headers,
+      body: jsonData,
+    );
+
+    if (response.statusCode == 200) {
+      print('Request successful');
+      print('Response: ${response.body}');
+    } else {
+      print('Request failed with status: ${response.statusCode}');
+    }
+  } catch (e) {
+    print('Error: $e');
   }
 }
