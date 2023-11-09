a10bedf4c65b95aeffe39ccc9f96032c2ca4ea79
Harshit Singh <89565061+harshitsingh09@users.noreply.github.com>
2023-11-09 20:13:05 +0530

    Update models.py

--- fast/models.py
+++ fast/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Boolean, Integer, String, Column
+from sqlalchemy import Boolean, Integer, String, Column, Float, ForeignKey
 from database import Base
 
 class User(Base):
@@ -8,5 +8,15 @@ class User(Base):
     email = Column(String(50), unique= True)
     password = Column(String(50))
 
+class menu(Base):
+    __tablename__ = 'menu'
+    item_ID = Column(Integer, primary_key= True, index= True)
+    item_name = Column(String(50), unique= True)
+    item_price = Column(Float)
 
-
+class Orders(Base):
+    __tablename__ = 'orders'
+    order_id = Column(Integer, primary_key= True, index= True)
+    item_id = Column(Integer, ForeignKey(menu.item_ID, ondelete= 'CASCADE'), nullable= False)
+    user_name = Column(String(50), ForeignKey(User.username, ondelete= 'CASCADE'), nullable= False)
+    quantity = Column(Integer)
